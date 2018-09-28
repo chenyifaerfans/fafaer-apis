a6f690d67fbb7d035844639ccac632ac3326e90c
upcwangying <upcwangying@126.com>
2018-09-28 21:26:26 +0800

    修复表单上传文件类型验证bug

--- apps/common/validations.py
+++ apps/common/validations.py
@@ -7,35 +7,38 @@ from rest_framework import serializers
 
 
 def validate_fields(field, upload_max_size=0, upload_types=[]):
-    names = getattr(field, 'name').split(".")
-    field_name = getattr(field, 'field_name')
-    if len(names) == 2:
-        file_type = names[1]
-        if not upload_types or file_type.lower() in upload_types:
-            if not upload_max_size or getattr(field, 'size') <= upload_max_size:
-                return field
-            else:
-                raise serializers.ValidationError(_("文件大小不能大于%sMB" % str(upload_max_size / 1024 / 1024)),
-                                                  code=_("%s_invalid" % field_name))
+    file_name = getattr(field, 'name')
+    names = file_name.rsplit(".", 1)
+    if len(names) != 2:
+        raise serializers.ValidationError(_("文件名错误"), code=_("%s_invalid" % file_name))
+
+    field_name = names[0]
+    file_type = names[1]
+    if not upload_types or file_type.lower() in upload_types:
+        if not upload_max_size or getattr(field, 'size') <= upload_max_size:
+            return field
         else:
-            raise serializers.ValidationError(_("文件必须为'%s'格式" % ','.join(upload_types)),
+            raise serializers.ValidationError(_("文件大小不能大于%sMB" % str(upload_max_size / 1024 / 1024)),
                                               code=_("%s_invalid" % field_name))
     else:
-        raise serializers.ValidationError(_("文件名错误"), code=_("%s_invalid" % field_name))
+        raise serializers.ValidationError(_("文件必须为'%s'格式" % ','.join(upload_types)),
+                                          code=_("%s_invalid" % field_name))
 
 
 def clean_fields(field, upload_max_size=0, upload_types=[]):
-    names = getattr(field, 'name').split(".")
-    field_name = getattr(field, 'field_name')
-    if len(names) == 2:
-        file_type = names[1]
-        if not upload_types or file_type.lower() in upload_types:
-            if not upload_max_size or getattr(field, 'size') <= upload_max_size:
-                return field
-            else:
-                raise forms.ValidationError(_("文件大小不能大于%sMB" % str(upload_max_size / 1024 / 1024)),
-                                            code=_("%s_invalid" % field_name))
+    file_name = getattr(field, 'name')
+    names = file_name.rsplit(".", 1)
+    if len(names) != 2:
+        raise forms.ValidationError(u"文件名错误", code=_("%s_invalid" % file_name))
+
+    field_name = names[0]
+    file_type = names[1]
+    if not upload_types or file_type.lower() in upload_types:
+        if not upload_max_size or getattr(field, 'size') <= upload_max_size:
+            return field
         else:
-            raise forms.ValidationError(_("文件必须为'%s'格式" % ','.join(upload_types)), code=_("%s_invalid" % field_name))
+            raise forms.ValidationError(_("文件大小不能大于%sMB" % str(upload_max_size / 1024 / 1024)),
+                                        code=_("%s_invalid" % field_name))
     else:
-        raise forms.ValidationError(u"文件名错误", code=_("%s_invalid" % field_name))
+        raise forms.ValidationError(_("文件必须为'%s'格式" % ','.join(upload_types)), code=_("%s_invalid" % field_name))
+
