4491ae66c7a8afe3651daefe5b775397e8aeb151
upcwangying <upcwangying@126.com>
2018-12-23 20:30:53 +0800

    表结构变更

--- apps/videos/models.py
+++ apps/videos/models.py
@@ -13,6 +13,9 @@ class VideoCollection(models.Model):
     name = models.CharField(max_length=30, unique=True, verbose_name="视频合集名")
     desc = models.CharField(max_length=100, verbose_name=u"描述")
     date = models.DateField(default=datetime.now, verbose_name=u'日期')
+    cover_img = models.ImageField(upload_to="video/collection/cover/%Y/%m", max_length=100, verbose_name='视频集图片')
+    background_img = models.ImageField(upload_to="video/collection/background/%Y/%m",
+                                       max_length=100, verbose_name='视频集背景图片')
     user = models.ForeignKey(User, verbose_name=u"创建用户")
 
     add_time = models.DateTimeField(default=datetime.now, verbose_name=u"创建时间")
@@ -36,7 +39,7 @@ class Video(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False, verbose_name=u"主键")
     name = models.CharField(max_length=30, unique=True, verbose_name="视频名称")
     desc = models.CharField(max_length=100, verbose_name=u"视频描述")
-    file = models.FileField(upload_to="video/%Y/%m", verbose_name=u"视频文件")
+    file = models.FileField(upload_to="video/collection/%Y/%m", verbose_name=u"视频文件")
     date = models.DateField(default=datetime.now, verbose_name=u'视频日期')
     user = models.ForeignKey(User, verbose_name=u"创建用户")
 
